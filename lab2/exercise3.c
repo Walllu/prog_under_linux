f5f307ad9831d16ff2015525872a68300d6e758b
Waltteri Leinonen <2270077l@student.gla.ac.uk>
2018-01-31 16:56:58 +0000

    finished ex 3 for C

--- lab2/exercise3.c
+++ lab2/exercise3.c
@@ -10,19 +10,23 @@ int main(void){
 	do {
 	int inpnum;
 	char again, buffer[50];
+	printf("\n");
 	printf("Enter a number to see if it's \'square-free\':\n");
 	fgets(buffer, sizeof(buffer), stdin);
 	sscanf(buffer, "%d", &inpnum);
 	double sq = floor(sqrt(inpnum)); // the largest square underneath inpnum
 	printf("sq %f\n", sq);
-	if(fmod(sqrt(inpnum),1)==0){
+	// if the number you typed in is a square
+	if(fmod(sqrt(inpnum),1)==0||inpnum==2){
 		printf("You typed in a square!\n");
-	} else {
+	} else{
 		int found = 0; // a boolean flag, to see if we found a square divisor orn ot
-		for(int i=1;i<=sq;i++){
-			printf("%d -- %d\n",i, inpnum);
-			if(inpnum%(i^2)==0){
-				printf("The square %d divides %d ..\n", i^2, inpnum);
+		// for every number less that the largest possible square divisor
+		for(int i=2;i<=sq;i++){
+			printf("%d\n",i);
+			if(fmod(inpnum,i*i)==0){
+				printf("-----%d\n",i);
+				printf("The square %d divides %d ..\n", i*i, inpnum);
 				found = 1;
 			break;
 			}
